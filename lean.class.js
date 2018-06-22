8e887e2299f34473ad303e45e5ac1162e519d2a5
sanyabeast <a.gvrnsk@gmail.com>
2018-06-22 17:13:21 +0300

    Major functionality

---
+++ lean.class.js
@@ -0,0 +1,20 @@
+window.Class = window.Class || function(name, description){
+	if (typeof name == "object"){
+		description = name;
+		name = "Class";
+	}
+
+	if (typeof description !== "object" || Array.isArray(description)){
+		throw new Error("Invalid class description");
+	}
+
+	var _constructor = description.$constructor;
+	delete description.$constructor;
+	var $constructor = eval([
+		"var " + name + " = function(){",
+		"	if (_constructor) _constructor.apply(this, arguments);",
+		"};" + name
+	].join(""));
+	$constructor.prototype = Object.create(description);
+	return $constructor;
+};
